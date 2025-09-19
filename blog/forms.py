b3ace9316195405205e97216105c4b67f8aa6211
DylanAustin-TheDreamer <austindylan0@gmail.com>
2025-09-19 13:34:52 +0100

    Created edit and delete comments

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,11 @@
-from .models import Comment
+from .models import CollaborateRequest, Comment
 from django import forms
+
+
+class CollaborateForm(forms.ModelForm):
+    class Meta:
+        model = CollaborateRequest
+        fields = ('name', 'email', 'message')
 
 
 class CommentForm(forms.ModelForm):
